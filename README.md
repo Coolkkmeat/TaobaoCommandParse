9fa03dc099acfcacdd86483371790bb419a1f88d
Coolkk <2411947017@qq.com>
2020-09-16 17:37:55 +0800

    Taobao Command Parse

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# TaobaoCommandParse
-油猴脚本：将淘宝生成的淘口令转换为链接
+## 介绍
+这是一个可以将淘宝生成的淘口令转换为链接的工具。
+
+## 教程
+1. [安装脚本](https://greasyfork.org/scripts/411432)
+2. 打开[淘宝官网](https://www.taobao.com/)
+3. 将带有`￥`或`$`字符的淘口令输入或粘贴到搜索框中
+4. 自动解析并打开对应链接
+
+![图片_1](.github/image_1.jpg)
+
+## 数据源
+解析功能的接口来自[淘发客](https://www.taofake.com/tools/tkljm/)。
+
+## 源码
+- [Github](https://github.com/Coolkkmeat/TaobaoCommandParse)
---
+++ src/main.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name            淘口令解析
+// @name:en         Taobao Command Parse
+// @namespace       net.coolkk.taobaocommandparse
+// @description     将淘宝生成的淘口令转换为链接
+// @description:en  Taobao Command Parse conversion into the link
+// @version         1.0.0
+// @author          Coolkk
+// @icon            https://img.alicdn.com/tps/i3/T1OjaVFl4dXXa.JOZB-114-114.png
+// @homepage        https://github.com/Coolkkmeat/TaobaoCommandParse
+// @supportURL      https://github.com/Coolkkmeat/TaobaoCommandParse/issues
+// @contributionURL https://coolkk.net/
+// @license         Apache License 2.0
+// @charset		    UTF-8
+// @require         https://cdn.bootcdn.net/ajax/libs/jquery/1.10.0/jquery.min.js
+// @include         http*://*taobao.com/*
+// @grant           GM_xmlhttpRequest
+// @connect         taofake.com
+// @run-at          document-idle
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    $("#q").on("input propertychange", function () {
+        var code = $("#q").val().match(/(￥|\$)([a-zA-Z0-9]*)(￥|\$)/)[2];
+        if (code) {
+            GM_xmlhttpRequest({
+                url: "//www.taofake.com/index/tools/gettkljm.html?tkl=" + code,
+                method: "GET",
+                responseType: "json",
+                timeout: 10000,
+                onload: function (res) {
+                    res = JSON.parse(res.responseText);
+                    if (res.code == 1) {
+                        $(window).attr("location", res.data.url);
+                    }
+                }
+            });
+        }
+    });
+})();
